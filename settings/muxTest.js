ee838b306c5f45d2d06b067ef3c972b87d7143c1
Henry Hunt <henhen7.24@gmail.com>
2021-08-19 15:22:31 -0500

    Tempurature Reads fully working

---
+++ settings/muxTest.js
@@ -0,0 +1,42 @@
+const Gpio = require('onoff').Gpio;
+
+const MUX1_EN_PIN_NUM = 6;
+const MUX2_EN_PIN_NUM = 12;
+
+const MUX_A0_PIN_NUM = 19;
+const MUX_A1_PIN_NUM = 20;
+const MUX_A2_PIN_NUM = 21;
+
+
+const mux1En = new Gpio(MUX1_EN_PIN_NUM, 'out');
+const mux2En = new Gpio(MUX2_EN_PIN_NUM, 'out');
+const muxA0 = new Gpio(MUX_A0_PIN_NUM, 'out');
+const muxA1 = new Gpio(MUX_A1_PIN_NUM, 'out');
+const muxA2 = new Gpio(MUX_A2_PIN_NUM, 'out');
+
+
+function sleep(ms) {
+    return new Promise((accept, reject) => setTimeout(accept, ms));
+}
+
+
+async function setMuxAddr(channel) {
+    var address = channel & 0x0F;
+    // use the leading digit to determine weather the channel 
+    // is on the first or second mux
+    mux1En.writeSync(Number(!(address & (1 << 3))));
+    mux2En.writeSync(Number(!!(address & (1 << 3))));
+
+    console.log(`Set the Mux to ${mux2En.readSync()} ${mux1En.readSync()}`);
+    console.log(`Should be ${!!(address & (1 << 3))} ${!(address & (1 << 3))}`);
+    // now using the least sigificant bits to get the mux input bits
+    muxA2.writeSync(Number(!!(address & (1 << 2))));
+    muxA1.writeSync(Number(!!(address & (1 << 1))));
+    muxA0.writeSync(Number(!!(address & 1)));
+    console.log(`Set the Mux Address to ${muxA2.readSync()} ${muxA1.readSync()} ${muxA0.readSync()}`)
+}
+
+
+setMuxAddr(0);
+setMuxAddr(15);
+
--- settings/spiTest.js
+++ settings/spiTest.js
@@ -57,6 +57,21 @@ TCAmp.transferSync(message)
 console.log(message);
 
 
+
+function convertToCel(tempBuffer) {
+	const sign = -1 * Number(!!(tempBuffer[1] & 1 << 7)) + Number(!(tempBuffer[1] & 1 << 7));
+	const HighByte = (tempBuffer[1] & 0b01111111) << 4;
+	const MidByte = tempBuffer[2]*Math.pow(2, -4);
+	const LowByte = (tempBuffer[3] & 0b11100000)*Math.pow(2, -12);
+
+	console.log(`Sign ${sign} High Byte ${HighByte} Mid Byte ${MidByte} Low Byte ${LowByte}`);
+
+	return sign*(HighByte + MidByte + LowByte);
+}
+
+console.log(convertToCel(message[0].receiveBuffer));
+
+
 message = [{
 	sendBuffer: Buffer.from([0x0a, 0x00, 0x00]),
 	receiveBuffer: Buffer.alloc(3),
@@ -68,4 +83,26 @@ TCAmp.transferSync(message)
 console.log(message);
 
 
+function convertCjToCel(tempBuffer){
+	const sign = -1 * Number(!!(tempBuffer[1] & 1 << 7)) + Number(!(tempBuffer[1] & 1 << 7));
+	const HighByte = tempBuffer[1] & 0b01111111;
+	const LowByte = tempBuffer[2]*Math.pow(2, -8);
+
+	console.log(`Sign ${sign} High Byte ${HighByte} Low Byte ${LowByte}`);
+
+	return sign*(HighByte + LowByte);
+}
+
+console.log(convertCjToCel(message[0].receiveBuffer));
+
+
+message = [{
+	sendBuffer: Buffer.from([0x0f, 0x00]),
+	receiveBuffer: Buffer.alloc(2),
+	byteLength: 2
+}]
+
+TCAmp.transferSync(message)
+
+console.log(message);
 
